d28f652dbbe89e7e8ea57ef4da9abadc97162bd6
Daito Akimura <>
2018-12-16 16:42:18 +0900

    add comments, reformat, move some methods to staticmethods

--- pymarketstore/client.py
+++ pymarketstore/client.py
@@ -1,9 +1,11 @@
 from __future__ import absolute_import
+
+import logging
+import re
+
 import numpy as np
 import pandas as pd
-import re
 import requests
-import logging
 import six
 
 from .jsonrpc import JsonRpcClient, MsgpackRpcClient
@@ -11,7 +13,6 @@ from .results import QueryReply
 from .stream import StreamConn
 
 logger = logging.getLogger(__name__)
-
 
 data_type_conv = {
     '<f4': 'f',
@@ -22,13 +23,12 @@ data_type_conv = {
 
 
 def isiterable(something):
+    """
+    check if something is a list, tuple or set
+    :param something: any object
+    :return: bool. true if something is a list, tuple or set
+    """
     return isinstance(something, (list, tuple, set))
-
-
-def get_rpc_client(codec='msgpack'):
-    if codec == 'msgpack':
-        return MsgpackRpcClient
-    return JsonRpcClient
 
 
 def get_timestamp(value):
@@ -67,19 +67,40 @@ class Params(object):
         content = ('tbk={}, start={}, end={}, '.format(
             self.tbk, self.start, self.end,
         ) +
-            'limit={}, '.format(self.limit) +
-            'limit_from_start={}'.format(self.limit_from_start))
+                   'limit={}, '.format(self.limit) +
+                   'limit_from_start={}'.format(self.limit_from_start))
         return 'Params({})'.format(content)
 
 
 class Client(object):
 
     def __init__(self, endpoint='http://localhost:5993/rpc'):
+        """
+        initialize the MarketStore client with specified server endpoint
+        :param endpoint:
+        """
         self.endpoint = endpoint
-        rpc_client = get_rpc_client('msgpack')
+        rpc_client = self._get_rpc_client('msgpack')
         self.rpc = rpc_client(self.endpoint)
 
+    @staticmethod
+    def _get_rpc_client(codec='msgpack'):
+        """
+        get an RPC client in specified codec
+        :param codec: currently supporting 'msgpack' only
+        :return: RPC client
+        """
+        if codec == 'msgpack':
+            return MsgpackRpcClient
+        return JsonRpcClient
+
     def _request(self, method, **query):
+        """
+        execute a request to MarketStore server
+        :param method: method name in string (ex. 'DataService.Query', 'DataService.ListSymbols')
+        :param query:
+        :return:
+        """
         try:
             resp = self.rpc.call(method, **query)
             resp.raise_for_status()
@@ -90,30 +111,32 @@ class Client(object):
             raise
 
     def query(self, params):
-        if not isiterable(params):
-            params = [params]
-        query = self.build_query(params)
+        """
+        execute QUERY to MarketStore server
+        :param params: Params object used to query
+        :return: QueryReply object
+        """
+
+        query = self._build_query(params)
         reply = self._request('DataService.Query', **query)
+
         return QueryReply(reply)
 
     def write(self, recarray, tbk, isvariablelength=False):
-        data = {}
-        data['types'] = [
-            recarray.dtype[name].str.replace('<', '')
-            for name in recarray.dtype.names
-        ]
-        data['names'] = recarray.dtype.names
-        data['data'] = [
-            bytes(buffer(recarray[name])) if six.PY2
-                else bytes(memoryview(recarray[name]))
-            for name in recarray.dtype.names
-        ]
-        data['length'] = len(recarray)
-        data['startindex'] = {tbk: 0}
-        data['lengths'] = {tbk: len(recarray)}
+        """
+        execute WRITE to MarketStore server
+        :param recarray: numpy.array object to write
+        :param tbk: Time Bucket Key string.
+        ('{symbol name}/{time frame}/{attribute group name}' ex. 'TSLA/1Min/OHLCV' , 'AAPL/1Min/TICK' )
+        :param isvariablelength: should be set true if the record content is variable-length array
+        :return:
+        """
+        data = self._build_data(recarray, tbk)
+
         write_request = {}
         write_request['dataset'] = data
         write_request['isvariablelength'] = isvariablelength
+
         writer = {}
         writer['requests'] = [write_request]
         try:
@@ -125,7 +148,37 @@ class Client(object):
         resp = self.rpc.response(reply_obj)
         return resp
 
-    def build_query(self, params):
+    @staticmethod
+    def _build_data(recarray, tbk):
+        """
+        build data for write
+        :param recarray: numpy.array object to write
+        :param tbk: Time Bucket Key string.
+        :return: data in dictionary
+        """
+        data = {}
+        data['types'] = [
+            recarray.dtype[name].str.replace('<', '')
+            for name in recarray.dtype.names
+        ]
+        data['names'] = recarray.dtype.names
+        data['data'] = []
+        for name in recarray.dtype.names:
+            data['data'].append(bytes(buffer(recarray[name])) if six.PY2
+                                else bytes(memoryview(recarray[name])))
+        data['length'] = len(recarray)
+        data['startindex'] = {tbk: 0}
+        data['lengths'] = {tbk: len(recarray)}
+
+        return data
+
+    @staticmethod
+    def _build_query(params):
+        """
+        build parameters for QUERY
+        :param params: pymarketstore.Params object
+        :return: request params in array
+        """
         reqs = []
         if not isiterable(params):
             params = [params]
@@ -151,12 +204,20 @@ class Client(object):
         }
 
     def list_symbols(self):
+        """
+        execute LIST SYMBOLS to MarketStore server
+        :return:
+        """
         reply = self._request('DataService.ListSymbols')
         if 'Results' in reply.keys():
             return reply['Results']
         return []
 
     def server_version(self):
+        """
+        get MarketStore server version in the 'Marketstore-Version' HTTP response headers.
+        :return: version string
+        """
         resp = requests.head(self.endpoint)
         return resp.headers.get('Marketstore-Version')
 
