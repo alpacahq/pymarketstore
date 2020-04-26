be8f2bdd1015b875774d16f08f4ff0789b89ef90
Daito AKIMURA <dakimura@mercari.com>
2020-04-26 11:29:12 +0900

    fix: auto find_packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 import ast
 import re
-from setuptools import setup
+from setuptools import setup, find_packages
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
@@ -25,7 +25,7 @@ setup(
     author_email='oss@alpaca.markets',
     url='https://github.com/alpacahq/pymarketstore',
     keywords='database,pandas,financial,timeseries',
-    packages=['pymarketstore', ],
+    packages=find_packages(exclude=('tests', 'docs')),
     install_requires=[
         'msgpack-python',
         'numpy',
