019198711050111916e0d776721f0c3961d40946
Daito AKIMURA <dakimura@mercari.com>
2021-05-06 12:35:46 +0900

    fix: tiny fix of a bug made during merging a code

--- pymarketstore/jsonrpc.py
+++ pymarketstore/jsonrpc.py
@@ -48,7 +48,7 @@ class MsgpackRpcClient(object):
             return http_resp
 
         http_resp.raise_for_status()
-        return self.codec.loads(http_resp.content)
+        return msgpack.loads(http_resp.content)
 
     @staticmethod
     def _rpc_response(reply: Dict) -> str:
